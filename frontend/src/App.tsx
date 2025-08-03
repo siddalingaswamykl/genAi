eb193e03b51501b899c99e6e89ae304abf9b6e9b
siddalingaswamykl <siddalingaswamykl@gmail.com>
2025-08-03 18:02:32 +0530

    commit changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 import VehicleConfig from './components/VehicleConfig';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
       <h1>GenAI Vehicle Configurator</h1>
       <VehicleConfig />
     </div>
